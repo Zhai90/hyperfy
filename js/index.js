122288dd8af791b85a268b25a5a29a1ae9df52bb
zhai071 <zhaihongmeng@gmail.com>
2022-03-07 19:41:00 -0300

    getCookie is not defined

--- js/index.js
+++ js/index.js
@@ -20,13 +20,13 @@ const nav = {
     console.log("Attempted to redirect to Support!");
   },
   dropdown: function () {
-    let d = document.querySelector(".nav-link:nth-child(5)")
+    let d = document.querySelector(".nav-link:nth-child(5)");
     d.classList.toggle("dropdown");
-    if(d.classList.contains("dropdown")) {
-      let n = document.querySelector("nav")
+    if (d.classList.contains("dropdown")) {
+      let n = document.querySelector("nav");
       n.style.height = "26vh";
-    }else{
-      let n = document.querySelector("nav")
+    } else {
+      let n = document.querySelector("nav");
       n.style.height = "12vh";
     }
     console.log("Dropdowned");
@@ -55,10 +55,23 @@ for (i; i < length; i++) {
 }
 
 // use desktop mode or bald (this code saves you from being bald)
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
 
 var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+if (viewMode == "desktop") {
+  viewport.setAttribute("content", "width=1024");
+} else if (viewMode == "mobile") {
+  viewport.setAttribute(
+    "content",
+    "width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no"
+  );
 }
--- js/support.js
+++ js/support.js
@@ -41,6 +41,16 @@ window.onscroll = () => {
   }
 };
 // use desktop mode or bald (this code saves you from being bald)
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
 
 var viewMode = getCookie("view-mode");
 if (viewMode == "desktop") {
